6273f8b49b3f43b428e6732c8501956953347d46
Dale Foskey <dalefoskey@icloud.com>
2020-07-13 08:12:34 -0400

    created readme

--- README.md
+++ README.md
@@ -1,37 +1,25 @@
 
-  #Dales new project
+  # My ReadMe Generator
   ---
 
   ## Description
   ---
-  > this is a readme generator, that i created with node.js
+  > This is an automated professional readme.md generator
 
   ![screenshot] (screenshot.png)
 
 
   ## Getting Started
   ---
-  > you will do this to depoly
+  > first got to command line and while in the projects home directory, first run from a terminal with command: npm i -A , then afterwards run from comand line: node index, complete the required input, enjoy. Spend more time coding.....
 
 
   ## Installation
   ---
-  > better hurry up
+  > command line, node index
 
   To launch the program, write the following in the terminal window:
   node index
   
   
-
-
-
-
-
-
-
-
-
-
-
-
   
--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const util = require('util')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-const writeFileAsync = util.promisify(fs.writeFile)
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser () {
+function promptUser() {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'projectName',
-      message: 'What is your project name ?'
+      type: "input",
+      name: "projectName",
+      message: "What is your project name ?",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Project description?'
+      type: "input",
+      name: "description",
+      message: "Project description?",
     },
     {
-      type: 'input',
-      name: 'video',
-      message: 'location of screenshot or video ?'
+      type: "input",
+      name: "video",
+      message: "location of screenshot or video ?",
     },
     {
-      type: 'input',
-      name: 'gettingStarted',
-      message: 'Enter instructions on how to deploy your site?'
+      type: "input",
+      name: "gettingStarted",
+      message: "Enter instructions on how to deploy your site?",
     },
     {
-      type: 'input',
-      name: 'commanLine',
-      message: 'Enter command to start project'
+      type: "input",
+      name: "commanLine",
+      message: "Enter command to start project",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'Instructions to deploy site'
+      type: "input",
+      name: "installation",
+      message: "Instructions to deploy site",
     },
     {
-      type: 'input',
-      name: 'webpage',
-      message: 'Enter your webpage address.'
+      type: "input",
+      name: "webpage",
+      message: "Enter your webpage address.",
     },
     {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.'
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL.",
     },
     {
-      type: 'input',
-      name: 'twitter',
-      message: 'Enter your twitter handle.'
+      type: "input",
+      name: "twitter",
+      message: "Enter your twitter handle.",
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email ?'
-    }
-  ])
+      type: "input",
+      name: "email",
+      message: "What is your email ?",
+    },
+  ]);
 }
 
-function generateHTML (answers) {
-  projectname = answers.projectName
-  console.log(answers.projectName)
+function generateHTML(answers) {
+  projectname = answers.projectName;
+  console.log(answers.projectName);
   return `
   # ${answers.projectName}
   ---
@@ -86,22 +86,22 @@ function generateHTML (answers) {
   ${answers.commanLine}
   
   
-  `
+  `;
 }
 
-async function init () {
-  console.log('hi')
+async function init() {
+  console.log("hi");
   try {
-    const answers = await promptUser()
+    const answers = await promptUser();
 
-    const html = generateHTML(answers)
+    const html = generateHTML(answers);
 
-    await writeFileAsync('README.md', html)
+    await writeFileAsync("README.md", html);
 
-    console.log('Successfully wrote to README.md')
+    console.log("Successfully wrote to README.md");
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
-init()
+init();
