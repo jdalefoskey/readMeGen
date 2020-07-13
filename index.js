d71e2d00683f1e3b46e745264400971c7ff821aa
Dale Foskey <dalefoskey@icloud.com>
2020-07-13 08:44:45 -0400

    changes to template

--- index.js
+++ index.js
@@ -1,68 +1,73 @@
+// created required links
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// Promps user for questions, answers stored for retrieval
 function promptUser() {
   return inquirer.prompt([
     {
       type: "input",
-      name: "projectName",
+      name: "projectName", // Link to stored answer
       message: "What is your project name ?",
     },
     {
       type: "input",
-      name: "description",
+      name: "description", // Link to stored answer
       message: "Project description?",
     },
     {
       type: "input",
-      name: "video",
+      name: "video", // Link to stored answer
       message: "location of screenshot or video ?",
     },
     {
       type: "input",
-      name: "gettingStarted",
+      name: "gettingStarted", // Link to stored answer
       message: "Enter instructions on how to deploy your site?",
     },
     {
       type: "input",
-      name: "commanLine",
+      name: "commanLine", // Link to stored answer
       message: "Enter command to start project",
     },
     {
       type: "input",
-      name: "installation",
+      name: "installation", // Link to stored answer
       message: "Instructions to deploy site",
     },
     {
       type: "input",
-      name: "webpage",
+      name: "webpage", // Link to stored answer
       message: "Enter your webpage address.",
     },
     {
       type: "input",
-      name: "linkedin",
+      name: "linkedin", // Link to stored answer
       message: "Enter your LinkedIn URL.",
     },
     {
       type: "input",
-      name: "twitter",
+      name: "twitter", // Link to stored answer
       message: "Enter your twitter handle.",
     },
     {
       type: "input",
-      name: "email",
+      name: "email", // Link to stored answer
       message: "What is your email ?",
     },
   ]);
 }
-
+// generating readme.md document with contextual answers
 function generateHTML(answers) {
   projectname = answers.projectName;
   console.log(answers.projectName);
   return `
+  ![Alt text](https://res.cloudinary.com/practicaldev/image/fetch/s--7yVSltAE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://user-images.githubusercontent.com/9840435/59461914-cbc18380-8e22-11e9-8567-87b43da950ac.png?raw=true "readme")
+  ---
+  
   # ${answers.projectName}
   ---
 
@@ -70,15 +75,16 @@ function generateHTML(answers) {
   ---
   > ${answers.description}
 
-  ![screenshot] (screenshot.png)
+  ![Alt text](/${answers.video}?raw=true "Screen Shot")
+  
 
 
-  ## Getting Started
+  ### Getting Started
   ---
   > ${answers.gettingStarted}
 
 
-  ## Installation
+  ### Installation
   ---
   > ${answers.installation}
 
@@ -88,7 +94,8 @@ function generateHTML(answers) {
   
   `;
 }
-
+// and here is where the magic all comes together, writes the file to disk,
+// and confirms the file was created
 async function init() {
   console.log("hi");
   try {
@@ -98,10 +105,10 @@ async function init() {
 
     await writeFileAsync("README.md", html);
 
-    console.log("Successfully wrote to README.md");
+    console.log("No sweat README.md is done!");
   } catch (err) {
     console.log(err);
   }
 }
-
+//  Here is where we kick everything off
 init();
